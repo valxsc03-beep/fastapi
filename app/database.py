388542809b0302af3f9de08b60f9b5b51981756f
valxsc03-beep <valxsc03@email.com>
2026-06-17 17:33:50 -0500

    version antes de la conexion de base de datos con sqlmodel

---
+++ app/database.py
@@ -0,0 +1,9 @@
+from sqlmodel import SQLModel, create_engine
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url)
+
+def crear_bd():
+    SQLModel.metadata.create_all(engine)
