f30560da679f696db0d6b6cf6fb97402b207e52c
valxsc03-beep <valxsc03@email.com>
2026-07-01 16:05:48 -0500

    Configuración de SessionDependency para SQLModel

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
-from sqlmodel import SQLModel, create_engine
+from typing import Annotated
+
+from fastapi import Depends
+from sqlmodel import SQLModel, Session, create_engine
 
 sqlite_file_name = "database.db"
-
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(
@@ -9,5 +11,14 @@ engine = create_engine(
     connect_args={"check_same_thread": False}
 )
 
+
 def crear_bd():
     SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDependency = Annotated[Session, Depends(get_session)]
