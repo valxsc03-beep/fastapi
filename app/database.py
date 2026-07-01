486d32f02ea4a8c81726fa6abe24747497ba21cb
valxsc03-beep <valxsc03@email.com>
2026-07-01 15:57:44 -0500

    Configuración inicial de SQLite y creación automática de tablas

--- app/database.py
+++ app/database.py
@@ -1,9 +1,13 @@
 from sqlmodel import SQLModel, create_engine
 
 sqlite_file_name = "database.db"
+
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-engine = create_engine(sqlite_url)
+engine = create_engine(
+    sqlite_url,
+    connect_args={"check_same_thread": False}
+)
 
 def crear_bd():
     SQLModel.metadata.create_all(engine)
